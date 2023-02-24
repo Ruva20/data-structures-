a593fd99df534e67ff461a27430d7ab7e6ae1527
Ruva20 <125276648+Ruva20@users.noreply.github.com>
2023-02-24 13:55:44 +0000

    Add files via upload
    6950721 Ruvarashe Mbizvo

---
+++ ASS 2 CARS.py
@@ -0,0 +1,41 @@
+#script for car dealers
+#carBrand with their respective prices in GHc
+carPrice = {'BUICK' : 6000,
+'HONDA' : 7000,
+'INFINITI' : 8000,
+'MAZDA' : 9000,
+'ACUARA' : 10000,#
+'LINCOLN' : 11000,
+'VOLVO' : 12000,
+'HYUNDAI': 13000,
+'TOYOTA' : 14000,
+'KIA' : 15000,
+'SCION' : 16000,
+'CHEVROLET' : 17000,
+'CADILLAC' : 18000,
+'AUDI' : 19000,
+'BMW' : 20000,
+'VOLKSWAGEN' :21000,
+'LEXUS' : 22000,
+'PORSCHE' :23000,
+'FORD' : 24000,
+'GMC' : 25000,
+'CHRYSLER' : 26000,
+'NISSAN' : 27000,
+'JAGUAR': 28000,
+'MITSUBISHU' : 29000,
+'MERCEDES-BENZ' : 30000,
+'DODGE': 31000,
+'RAM' : 32000,
+'MINI' : 33000,
+'TESLA': 34000,
+'RENAULT' : 35000,
+'HOLDEN' : 36000, }
+name = input('Please specify the carbrand of your choice:')
+print('Your prefered carbrand is',name, 'which cost',carPrice[name],'.00 GHc')
+
+GITHUB LINK:https://github.com/Ruva20
+      
+
+ 
+
